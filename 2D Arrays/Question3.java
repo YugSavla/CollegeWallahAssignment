189a716ced5186b59db45ed33870d090447671c1
YugSavla <yugsavla1@gmail.com>
2024-06-26 16:02:00 +0530

    2D Arrays

--- 2D Arrays/Question3.java
+++ 2D Arrays/Question3.java
@@ -1,3 +1,32 @@
+import java.util.Scanner;
+
 public class Question3 {
-    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of rows");
+        int m=sc.nextInt();
+        System.out.println("Enter the number of columns");
+        int n=sc.nextInt();
+        int arr[][]=new int[m][n];
+        System.out.println("Enter the element");
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j <n; j++) {
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        int[][] cord=new int[2][2];
+        System.out.println("Enter the cordinate");
+        for (int i = 0; i < cord.length; i++) {
+            for (int j = 0; j < cord.length; j++) {
+                cord[i][j]=sc.nextInt();
+            }
+        }
+        int sum=0;
+        for (int   i = cord[0][0]; i <=cord[1][0] ; i++) {
+            for (int j = cord[0][1]; j <= cord[1][1]; j++) {
+                sum+=arr[i][j];
+            }
+        }
+        System.out.println("Sum is "+sum);        
+    }
 }
