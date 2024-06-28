5215a59bd9e03f5b37043f7ad6342eb8e4e06809
YugSavla <yugsavla1@gmail.com>
2024-06-28 20:27:44 +0530

    String

---
+++ String/Question1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Question1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        char ch[]=new char[1];
+        for (int i = 0; i < ch.length; i++) {
+            sc.next().charAt(0);
+        }
+    }
+}
